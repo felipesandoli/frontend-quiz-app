88fd899129c1805ad199ae6ae0958beef51dc8a4
felipesandoli <cfelipesandoli@gmail.com>
2024-02-24 18:56:09 +0000

    Add light model icons and toggle switch

---
+++ app/UI/mode-toggle.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+
+export default function ModeToggle() {
+    return (
+        <div>
+
+            <Image 
+                src="/icon-sun-dark.svg"
+                width={24}
+                height={24}
+                alt='Light mode icon'
+                />
+            <label htmlFor="" className="inline-flex items-center cursor-pointer">
+                <input type="checkbox" value="" className="sr-only peer" />
+                <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
+            <Image 
+                src="/icon-moon-dark.svg"
+                width={24}
+                height={24}
+                alt='Dark mode icon'
+            />
+        </div>
+    )
+}
