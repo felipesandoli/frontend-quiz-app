9b4dedd7d6c5203d9c895f5d881e264fc67d047b
felipesandoli <cfelipesandoli@gmail.com>
2024-03-02 17:36:50 +0000

    Move up theme state handling to page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,29 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from 'react'
 import Header from "@/app/ui/header"
 
 export default function Home() {
+  const [theme, setTheme] = useState("light")
+
+  function toggleTheme(e) {
+    let body = document.getElementsByTagName("body")[0]
+
+    if (e.target.checked) {
+        setTheme('dark')
+        body.classList.remove("bg-light")
+        body.classList.add("bg-dark")
+    } else {
+        setTheme('light')
+        body.classList.remove("bg-dark")
+        body.classList.add("bg-light")
+    }
+  }
+
   return (
-    <main>
-      <Header />
+    <main className={`bg-${theme}`}>
+      <Header theme={theme} onChange={toggleTheme} />
 
     </main>
   );
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,25 +1,8 @@
-'use client'
-
 import Image from 'next/image'
 import { rubik } from '@/app/ui/fonts'
-import { useState } from 'react'
 
-export default function Header() {
-    const [theme, setTheme] = useState("light")
 
-    function toggleTheme(e) {
-        let body = document.getElementsByTagName("body")[0]
-
-        if (e.target.checked) {
-            setTheme('dark')
-            body.classList.remove("bg-light")
-            body.classList.add("bg-dark")
-        } else {
-            setTheme('light')
-            body.classList.remove("bg-dark")
-            body.classList.add("bg-light")
-        }
-    }
+export default function Header({theme, onChange}) {
 
     return (
         <div className='flex justify-between w-11/12 mt-4 md:w-5/6 md:mt-10 xl:w-4/5 xl:mt-20 m-auto'>
@@ -40,7 +23,7 @@ export default function Header() {
                     alt='Light mode icon'
                     />
                 <label className="inline-flex items-center cursor-pointer mx-4 ">
-                    <input type="checkbox" className="sr-only peer" onChange={toggleTheme}/>
+                    <input type="checkbox" className="sr-only peer" onChange={onChange}/>
                     <div className="relative w-11 h-6  peer-focus:outline-none peer-focus:ring-4  rounded-full peer bg-purple-icon peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600"></div>
                 </label>
                 <Image 
