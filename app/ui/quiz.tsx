364456cb0f04534a20760811470e1cd9c9d5f993
felipesandoli <cfelipesandoli@gmail.com>
2024-03-06 21:32:12 +0000

    Style submit button hover effect

--- app/ui/quiz.tsx
+++ app/ui/quiz.tsx
@@ -3,17 +3,28 @@ import Image from 'next/image'
 import { useState } from 'react'
 import quizzes from './data.json'
 
-export default function Quiz({theme}) {
+export default function Quiz({theme}:any) {
     const [topic, setTopic] = useState('')
     const [questionNumber, setQuestionNumber] = useState(0)
+    const [selectedAnswer, setSelectedAnswer] = useState('')
+    const [isAnswerSubmitted, setIsAnswerSubmitted] = useState(false)
+    const [score, setScore] = useState(0)
 
     const quiz = topic ? (quizzes.quizzes.filter((quiz) => quiz.title === topic)[0].questions) : ([{question: '', options: [], answer: ''}]) 
     const { question, options, answer } = quiz[questionNumber]
     const optionLetters = ["A", "B", "C", "D"]
     
-    function handleTopicSelection(topic) {
+    function handleTopicSelection(topic:any) {
         setTopic(topic.code)
     }
+
+    // function submitAnswer() {
+    //     if (selectedAnswer === answer) {
+    //         setScore(score+1)
+    //         setQuestionNumber(questionNumber+1)
+    //     }
+    //     setIsAnswerSubmitted(true)
+    // }
 
     const topics = [
         {
@@ -43,13 +54,13 @@ export default function Quiz({theme}) {
             </div>
             <div className='grid gap-y-3 md:gap-y-6'>
                 {topics.map(topic => 
-                    <button className={`flex w-full p-3 xl:p-5 btn btn-${theme}`} key={topic.code} onClick={() => handleTopicSelection(topic)}>
+                    <button className={`flex w-full p-3 xl:p-5 btn btn-${theme} `} key={topic.code} onClick={() => handleTopicSelection(topic)}>
                         <Image
                             src={`/icon-${topic.code}.svg`}
                             width={40}
                             height={40}
                             alt='html'
-                            className={`topic-icon icon-${topic.code}`}
+                            className={`icon-topic icon-${topic.code}`}
                         />
                         <span className={`ml-4 md:ml-8 heading-s text-${theme}`}>{topic.screen}</span>
                     </button>
@@ -58,29 +69,30 @@ export default function Quiz({theme}) {
         </div>
          ) :
          (
-        <div className="pt-16 md:pt-0 xl:grid xl:grid-cols-2 w-5/6 m-auto">
+        <div className="pt-16 md:pt-0 grid grid-cols-1 xl:grid-cols-2 w-5/6 m-auto">
             <div className='xl:w-3/4 flex flex-col justify-between'>
                 <div>
                     <p className={`${rubik.className} body-s s-text-${theme} mt-4 md:mt-16`}>{`Question ${questionNumber + 1} of 10.`}</p>
                     <h1 className={`${rubik.className} heading-m text-${theme}`}>{question}</h1>
                 </div>
-                <div className={`w-100 p-1 progress-bar progress-bar-${theme}`}>
+                <div className={`w-100 p-1 mt-6 mb-10 xl:mb-0 progress-bar progress-bar-${theme}`}>
                     {/* On each increment of questionNumber, progress bar goes up 10% as there are 10 questiosn in list of questions  */}
                     <div className={`w-${(questionNumber+1)*10}  progress-bar-fill`}></div>
                 </div>
             </div>
-            <ul className='grid gap-y-3 md:gap-y-6' >
+            <div className='grid gap-y-3 md:gap-y-6' >
                 {options.map((option, index) => 
-                    <li key={index} >
-                        <button className={`flex w-full p-3 xl:p-5 btn btn-${theme}`}>
+                    <button className={`flex w-full p-3 xl:p-5 btn btn-${theme}`} key={index} onClick={() => setSelectedAnswer(option)}>
                         <div className={`grid justify-items-center w-10 h-10 md:w-14 md:h-14 option-icon heading-s`}>
                             {optionLetters[index]}                                
                         </div>
                         <span className={`ml-4 md:ml-8 heading-s text-${theme}`}>{option}</span>
-                        </button>
-                    </li>
+                    </button>
                 )}
-            </ul> 
+            </div>
+            <button className="xl:col-start-2 col-span-1 mt-3 md:mt-8 h-12 md:h-24 btn-submit heading-s">
+                    {isAnswerSubmitted ? "Next Question" : "Submit Answer"}
+            </button>
         </div>
          )
     )
